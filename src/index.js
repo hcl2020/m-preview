6de82007d2d516b446f653dddab41425b5dc1d79
hcl2020 <90795330@qq.com>
2017-06-07 14:59:28 +0800

    应用新的手机容器样式，代码重构

--- src/index.js
+++ src/index.js
@@ -1,59 +1,46 @@
 $(function () {
-    if( window.innerWidth > 800 ){
-        EnableMobileView();
+
+    function mPreview(option){
+        var strStyle='#m-preview{padding:50px;text-align:center}#m-preview-mobile{background:#222;display:inline-block;padding:0 4px;border-radius:26px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#000 6px 6px 20px 2px;border:#999 2px solid}#m-preview-mobile iframe{width:375px;height:667px;background:#fff;border:#000 2px solid;border-radius:5px;margin:0;padding:0;display:block}#m-preview-mobile p{margin:0;padding:20px 0;position:relative}#m-preview-mobile p mark{display:block;height:8px;margin:0 auto;width:70px;background:#444;-webkit-box-shadow:#a3a3a3 1px 1px 1px;box-shadow:#a3a3a3 1px 1px 1px;border-radius:5px}#m-preview-mobile p::before{content:" ";position:absolute;display:inline-block;top:16px;height:16px;width:16px;border-radius:50%;background:#ccc;-webkit-box-shadow:inset #080808 -8px 1px 7px 4px;box-shadow:inset #080808 -8px 1px 7px 4px;left:22%}#m-preview-mobile p::after{position:absolute;display:inline-block;content:" ";top:20px;height:8px;width:8px;border-radius:50%;background:#000;left:32%}#m-preview-mobile span{-webkit-box-shadow:#fff 1px 2px 3px;box-shadow:#fff 1px 2px 3px;display:block;height:36px;width:80px;border-radius:10px;background:rgba(0,0,0,0.3);margin:15px auto}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
+
+        var message = option.message || '建议使用手机访问此页面';
+        var pageUrl = location.href;
+        var strTpl = '<div id="m-preview"><div id="m-preview-mobile"><p><mark></mark></p><iframe src="' +
+            pageUrl +
+            '"></iframe><span></span></div><div id="m-preview-message">' +
+            message +
+            '</div></div><style>'+
+            strStyle
+            +'</style>';
+
+        var $body = document.getElementsByTagName('body')[0];
+        $body.innerHTML = strTpl;
+        // TODO 移除 head style 和 link style
+
+        var $iframe = document.getElementsByTagName('iframe')[0];
+        var _window = $iframe.contentWindow;
+        _window.onload = function() {
+            /* 处理滚动条 */
+            var strCss = '::-webkit-scrollbar{width:6px;height:5px;background-color:rgba(0,0,0,0.05)}'+
+                '::-webkit-scrollbar-thumb{border-radius:3px;background-color:rgba(0,0,0,0.3)}'+
+                '::-webkit-scrollbar-thumb:hover{border-radius:3px;background-color:rgba(0,0,0,0.7)}';
+            var $style = _window.document.createElement('style');
+            $style.innerHTML = strCss;
+            _window.document.getElementsByTagName('head')[0].append($style);
+            /* 处理地址栏 */
+
+
+
+        }
+
+
+
     }
+
+    if( window.innerWidth > 600 ){
+        mPreview({
+            message: '建议使用手机访问此页面, 或访问此页面的<a href="#">电脑版</a>'
+        });
+    }
+
 });
-function EnableMobileView(){
-    var $iframe = $('<iframe id="mobile-iframe">');
-    $iframe.attr({
-        'src': location.href,
-        'width': '375px',
-        'height': '667px'
-    });
-    $iframe.css({
-        'width': '375px',
-        'height': '667px',
-
-        'box-sizing': 'content-box',
-        'padding': '35px 6px 56px',
-        'margin': '20px 70px',
-
-        'background': '#333',
-        'border-radius': '16px',
-        'box-shadow': '0 0 21px 2px #000',
-    });
-
-    $('body').html('').append( $iframe );
-
-    $('body').append($('<div>建议使用手机访问,或访问电脑版</div>').css({
-        'position': 'fixed',
-        'bottom': '0',
-        'text-align': 'center',
-        'width': '100%',
-        'color': '#ccc',
-        'padding': '7px',
-        'background': 'rgba(0,0,0,0.5)'
-    }));
-    $('body').css('cursor','auto');
-
-    $('body').append($('<div></div>').css({
-        'position': 'fixed',
-        'top': '728px',
-        'left': '242px',
-        'border-radius': '50%',
-        'color': 'rgb(153, 153, 153)',
-        'padding': '20px',
-        'border': '1px #000 solid',
-        'box-shadow': '1px 2px 3px #fff'
-    }));
-
-    var _window = $iframe[0].contentWindow;
-    _window.onload = function() {
-        var sheet = _window.document.styleSheets[0];
-        /*滚动条 start*/
-        sheet.insertRule('::-webkit-scrollbar {width: 6px;height: 5px;background-color: rgba(0,0,0, 0.05);}', 0);
-        /*定义滑块 内阴影+圆角*/
-        sheet.insertRule('::-webkit-scrollbar-thumb {border-radius: 3px;background-color: rgba(0,0,0, 0.3);}', 0);
-        sheet.insertRule('::-webkit-scrollbar-thumb:hover {border-radius: 3px;background-color: rgba(0,0,0, 0.7);}', 0);
-    }
-}
