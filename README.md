6d1b1defd91108acae75c9b3292d330b0da9287f
hcl2020 <90795330@qq.com>
2017-06-08 11:02:58 +0800

    v2.0.0 模块化，提高速度和兼容性
    1，使用UMD模块化，需要显示调用，不再隐式调用
    2，增加配置项
    3，使用document.body判断 代替window.onload，提高速度和兼容性
    4，编辑README

--- README.md
+++ README.md
@@ -2,14 +2,21 @@
 
 > M-preview is a plugin for previewing mobile webpage on a large screen.
 
-##Install
+## Install
 
     npm install --save m-preview
 
-##Usage
+## Usage
 
     <script src="dist/m-preview.js"></script>
 
-##License
+    var mPreview = require('m-preview');
+
+    mPreview({
+        message: '建议使用手机访问此页面, 或访问此页面的<a href="#">电脑版</a>',
+        threshold: 800
+    });
+
+## License
 
 MIT © [HeChanglin](mailto://907953320@qq.com)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-(function(){
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.mPreview = factory();
+    }
+})(this, function() {
 
-    function mPreview(option){
-        var strStyle='#m-preview{padding:50px;text-align:center}#m-preview-mobile{background:#222;display:inline-block;padding:0 4px;border-radius:26px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#000 6px 6px 20px 2px;border:#999 2px solid}#m-preview-mobile iframe{width:375px;height:667px;background:#fff;border:#000 2px solid;border-radius:5px;margin:0;padding:0;display:block}#m-preview-mobile p{margin:0;padding:20px 0;position:relative}#m-preview-mobile p mark{display:block;height:8px;margin:0 auto;width:70px;background:#444;-webkit-box-shadow:#a3a3a3 1px 1px 1px;box-shadow:#a3a3a3 1px 1px 1px;border-radius:5px}#m-preview-mobile p::before{content:" ";position:absolute;display:inline-block;top:16px;height:16px;width:16px;border-radius:50%;background:#ccc;-webkit-box-shadow:inset #080808 -8px 1px 7px 4px;box-shadow:inset #080808 -8px 1px 7px 4px;left:22%}#m-preview-mobile p::after{position:absolute;display:inline-block;content:" ";top:20px;height:8px;width:8px;border-radius:50%;background:#000;left:32%}#m-preview-mobile span{-webkit-box-shadow:#fff 1px 2px 3px;box-shadow:#fff 1px 2px 3px;display:block;height:36px;width:80px;border-radius:10px;background:rgba(0,0,0,0.3);margin:15px auto}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
+    var strStyle='#m-preview{padding:50px;text-align:center}#m-preview-mobile{background:#222;display:inline-block;padding:0 4px;border-radius:26px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#000 6px 6px 20px 2px;border:#999 2px solid}#m-preview-mobile iframe{width:375px;height:667px;background:#fff;border:#000 2px solid;border-radius:5px;margin:0;padding:0;display:block}#m-preview-mobile p{margin:0;padding:20px 0;position:relative}#m-preview-mobile p mark{display:block;height:8px;margin:0 auto;width:70px;background:#444;-webkit-box-shadow:#a3a3a3 1px 1px 1px;box-shadow:#a3a3a3 1px 1px 1px;border-radius:5px}#m-preview-mobile p::before{content:" ";position:absolute;display:inline-block;top:16px;height:16px;width:16px;border-radius:50%;background:#ccc;-webkit-box-shadow:inset #080808 -8px 1px 7px 4px;box-shadow:inset #080808 -8px 1px 7px 4px;left:22%}#m-preview-mobile p::after{position:absolute;display:inline-block;content:" ";top:20px;height:8px;width:8px;border-radius:50%;background:#000;left:32%}#m-preview-mobile span{-webkit-box-shadow:#fff 1px 2px 3px;box-shadow:#fff 1px 2px 3px;display:block;height:36px;width:80px;border-radius:10px;background:rgba(0,0,0,0.3);margin:15px auto}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
 
-        var message = option.message || '建议使用手机访问此页面';
+
+    var isDOMContentLoaded = function(){
+        return !!document.body;
+    }
+
+
+    var defaultOpt = {
+        message: '建议使用手机访问此页面',
+        threshold: 800 // maxWidth: 800
+    }
+
+    var mPreview = function mPreview(opt){
+        opt = opt || {};
+        var message = opt.message || defaultOpt.message;
+        var threshold = opt.threshold || defaultOpt.threshold;
+        if( window.innerWidth <= threshold ){
+            return false;
+        }
+        if(!isDOMContentLoaded()){
+            setTimeout(mPreview, 25, opt);
+            return false;
+        }
+
         var pageUrl = location.href;
         var strTpl = '<div id="m-preview"><div id="m-preview-mobile"><p><mark></mark></p><iframe src="' +
             pageUrl +
@@ -52,12 +81,5 @@
         }
     }
 
-    window.onload= function(){
-        if( window.innerWidth > 600 ){
-            mPreview({
-                message: '建议使用手机访问此页面, 或访问此页面的<a href="#">电脑版</a>'
-            });
-        }
-    };
-
-})();
+    return mPreview;
+});
