e202f514920c832ebb011e6e7be1fba2bdea15ec
hcl2020 <90795330@qq.com>
2017-06-08 15:03:05 +0800

    v2.0.5 避免样式冲突，细节优化
    1，样式优化，尽量避免预览页面出现滚动条
    2，使用js严格模式
    3，移除预览页面原有的stylesheet，避免样式冲突

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@
         root.mPreview = factory();
     }
 })(this, function() {
+    'use strict';
 
-    var strStyle='#m-preview{padding:50px;text-align:center}#m-preview-mobile{background:#222;display:inline-block;padding:0 4px;border-radius:26px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#000 6px 6px 20px 2px;border:#999 2px solid}#m-preview-mobile iframe{width:375px;height:667px;background:#fff;border:#000 2px solid;border-radius:5px;margin:0;padding:0;display:block}#m-preview-mobile p{margin:0;padding:20px 0;position:relative}#m-preview-mobile p mark{display:block;height:8px;margin:0 auto;width:70px;background:#444;-webkit-box-shadow:#a3a3a3 1px 1px 1px;box-shadow:#a3a3a3 1px 1px 1px;border-radius:5px}#m-preview-mobile p::before{content:" ";position:absolute;display:inline-block;top:16px;height:16px;width:16px;border-radius:50%;background:#ccc;-webkit-box-shadow:inset #080808 -8px 1px 7px 4px;box-shadow:inset #080808 -8px 1px 7px 4px;left:22%}#m-preview-mobile p::after{position:absolute;display:inline-block;content:" ";top:20px;height:8px;width:8px;border-radius:50%;background:#000;left:32%}#m-preview-mobile span{-webkit-box-shadow:#fff 1px 2px 3px;box-shadow:#fff 1px 2px 3px;display:block;height:36px;width:80px;border-radius:10px;background:rgba(0,0,0,0.3);margin:15px auto}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
+    var strStyle='#m-preview{padding-top:50px;text-align:center}#m-preview-mobile{background:#222;display:inline-block;padding:0 4px;border-radius:26px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#000 6px 6px 20px 2px;border:#999 2px solid}#m-preview-mobile iframe{width:375px;height:667px;background:#fff;border:#000 2px solid;border-radius:5px;margin:0;padding:0;display:block}#m-preview-mobile p{margin:0;padding:20px 0;position:relative}#m-preview-mobile p mark{display:block;height:8px;margin:0 auto;width:70px;background:#444;-webkit-box-shadow:#a3a3a3 1px 1px 1px;box-shadow:#a3a3a3 1px 1px 1px;border-radius:5px}#m-preview-mobile p::before{content:" ";position:absolute;display:inline-block;top:16px;height:16px;width:16px;border-radius:50%;background:#ccc;-webkit-box-shadow:inset #080808 -8px 1px 7px 4px;box-shadow:inset #080808 -8px 1px 7px 4px;left:22%}#m-preview-mobile p::after{position:absolute;display:inline-block;content:" ";top:20px;height:8px;width:8px;border-radius:50%;background:#000;left:32%}#m-preview-mobile span{-webkit-box-shadow:#fff 1px 2px 3px;box-shadow:#fff 1px 2px 3px;display:block;height:36px;width:80px;border-radius:10px;background:rgba(0,0,0,0.3);margin:15px auto}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
 
 
     var isDOMContentLoaded = function(){
@@ -44,7 +45,6 @@
 
         var $body = document.body;
         $body.innerHTML = strTpl;
-        // TODO 移除 head style 和 link style
 
         var $iframe = document.getElementsByTagName('iframe')[0];
         $iframe.onload = function(){
@@ -55,7 +55,7 @@
                 '::-webkit-scrollbar-thumb:hover{border-radius:3px;background-color:rgba(0,0,0,0.7)}';
             var $style = _window.document.createElement('style');
             $style.innerHTML = strCss;
-            _window.document.getElementsByTagName('head')[0].append($style);
+            _window.document.head.appendChild($style);
             /* 处理地址栏 */
             var pathname = _window.location.pathname;
 
@@ -79,6 +79,18 @@
                 }
             }
         }
+
+        // 移除 head 中的 stylesheet
+        var $head = document.head;
+        var $heads = $head.children;
+        for(var i=0; $heads[i];){
+            if($heads[i]['type'] == 'text/css' || $heads[i]['rel'] == 'stylesheet'){
+                $head.removeChild( $heads[i] );
+            }else{
+                i++;
+            }
+        }
+
     }
 
     return mPreview;
