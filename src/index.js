dee1727d34167ba07e46f67b7ac3ba648f2ca785
hcl2020 <907953320@qq.com>
2019-02-15 11:19:29 +0800

    v2.1.0 修改为全面屏样式

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@
 })(this, function() {
     'use strict';
 
-    var strStyle='#m-preview{padding-top:50px;text-align:center}#m-preview-mobile{background:#222;display:inline-block;padding:0 4px;border-radius:26px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#000 6px 6px 20px 2px;border:#999 2px solid}#m-preview-mobile iframe{width:375px;height:667px;background:#fff;border:#000 2px solid;border-radius:5px;margin:0;padding:0;display:block}#m-preview-mobile p{margin:0;padding:20px 0;position:relative}#m-preview-mobile p mark{display:block;height:8px;margin:0 auto;width:70px;background:#444;-webkit-box-shadow:#a3a3a3 1px 1px 1px;box-shadow:#a3a3a3 1px 1px 1px;border-radius:5px}#m-preview-mobile p::before{content:" ";position:absolute;display:inline-block;top:16px;height:16px;width:16px;border-radius:50%;background:#ccc;-webkit-box-shadow:inset #080808 -8px 1px 7px 4px;box-shadow:inset #080808 -8px 1px 7px 4px;left:22%}#m-preview-mobile p::after{position:absolute;display:inline-block;content:" ";top:20px;height:8px;width:8px;border-radius:50%;background:#000;left:32%}#m-preview-mobile span{-webkit-box-shadow:#fff 1px 2px 3px;box-shadow:#fff 1px 2px 3px;display:block;height:36px;width:80px;border-radius:10px;background:rgba(0,0,0,0.3);margin:15px auto}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
-
+    var strStyle = '#m-preview{padding-top:50px;text-align:center}#m-preview-mobile{background:#333;display:inline-block;padding:3px;border-radius:20px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#666 8px 20px 26px;border:#333 1px solid}#m-preview-mobile iframe{width:375px;height:734px;background:#fff;border:#000 2px solid;border-radius:17px;margin:0;padding:0;display:block}#m-preview-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#m-preview-message a{color:#666}';
+    strStyle += '#m-preview-qrcode{font-size: 14px;color: #999;text-align: center;position: fixed;top: 50%;right: 0;transform: translateY(-50%);background: #fff;padding: 10px;}';
 
     var isDOMContentLoaded = function(){
         return !!document.body;
@@ -19,12 +19,14 @@
 
     var defaultOpt = {
         message: '建议使用手机访问此页面',
+        tips: '', // '扫描二维码用手机查看~',
         threshold: 800 // maxWidth: 800
     }
 
     var mPreview = function mPreview(opt){
         opt = opt || {};
         var message = opt.message || defaultOpt.message;
+        var tips = opt.tips || defaultOpt.tips;
         var threshold = opt.threshold || defaultOpt.threshold;
         if( window.innerWidth <= threshold ){
             return false;
@@ -35,13 +37,16 @@
         }
 
         var pageUrl = location.href;
-        var strTpl = '<div id="m-preview"><div id="m-preview-mobile"><p><mark></mark></p><iframe src="' +
+
+        var strTpl = '<div id="m-preview"><div id="m-preview-mobile"><iframe src="' +
             pageUrl +
-            '"></iframe><span></span></div><div id="m-preview-message">' +
+            // '"></iframe><span></span></div><div id="m-preview-message">' +
+            '"></iframe></div><div id="m-preview-message">' +
             message +
-            '</div></div><style>'+
-            strStyle
-            +'</style>';
+            '</div><div id="m-preview-qrcode"><div id="m-preview-qrcode-img"></div><p>' + tips + '</p></div></div><style>' +
+            strStyle +
+            '</style>';
+
 
         var $body = document.body;
         $body.innerHTML = strTpl;
